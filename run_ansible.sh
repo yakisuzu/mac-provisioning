e5d26bb195a1b40f278135a84556f4bb96f2b6e3
yakisuzu <yakisuzu@gmail.com>
2015-10-29 18:13:51 +0900

    role introduce

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 pushd `dirname $0` >/dev/null
 
-check_uname=`uname -s |awk "{print $1}"`
-f_name=
-if [ $check_uname == 'Darwin' ]; then
-  f_name='brew.yml'
-elif [ $check_uname == 'Linux' ]; then
-  f_name='package.yml'
-else
-  echo `uname -s` is not supported
-  exit 1
-fi
-
-ansible-playbook -i ./hosts ./script/${f_name} -vv
-
-unset check_uname
-unset f_name
+ansible-playbook -i ./hosts ./default.yml -vv
 
 popd >/dev/null
 read -p "enter"
