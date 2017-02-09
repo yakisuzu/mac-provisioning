778e843fc505f82a3882f86ce389818a76c1e690
yakisuzu <yakisuzu@gmail.com>
2017-02-09 15:01:27 +0900

    del timing

--- script/download.sh
+++ script/download.sh
@@ -3,8 +3,7 @@ pushd `dirname $0` >/dev/null
 
 if [ ! -d `pwd`/provisioning-ansible ]; then
   curl -JO https://codeload.github.com/yakisuzu/provisioning-ansible/zip/master
-  unzip provisioning-ansible-master.zip
-  rm -f provisioning-ansible-master.zip
+  unzip provisioning-ansible-master.zip && rm -f provisioning-ansible-master.zip
   mv provisioning-ansible-master provisioning-ansible
 else
   echo exists provisioning-ansible dir
